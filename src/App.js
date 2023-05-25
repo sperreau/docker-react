996b4937345d78ca1a16003c708f1d9c1e1e188b
Samuel Perreault <samuel.yperreault@gmail.com>
2023-05-25 14:03:22 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          We've tested and will save and reload<code>src/App.js</code>.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Well, test, well
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
